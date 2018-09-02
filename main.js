e1b4b45bd5439eb81d9211ea28635985823fdf16
Robert Fono <fonorobert@gmail.com>
2018-09-02 12:44:07 -0700

    add more excuses

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 var reasons = [
 	"It's not you, it's me.",
 	"I'm not ready for a relationship right now.",
-	"I need to focus on myself right now.",
+	"I need to focus on myself.",
 	"I think I need to be alone for a bit.",
 	"I'm just so busy with work right now.",
 	"I don't think we're a good match.",
 	"I'm just not feeling it.",
 	"You deserve someone better.",
-	"My dog needs all my love and attention now."
+	"My dog needs all my love and attention now.",
+	"You're too good for me.",
+	"I'm still getting over my ex.",
+	"I'm fully devoted to my digital nomad lifestyle."
 ]
 
 document.addEventListener("DOMContentLoaded", function(event) {
